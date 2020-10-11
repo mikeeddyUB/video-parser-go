4e3707888f5986c6795d2608ca6309e6edc43145
mikeeddyUB <meddy22@gmail.com>
2020-10-10 22:06:05 -0400

    kind of working

--- internal/main.go
+++ internal/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/disintegration/imaging"
 	"github.com/lijo-jose/gffmpeg/pkg/gffmpeg"
 	"github.com/lijo-jose/goutils/pkg/ffmpeg"
-	"github.com/otiai10/gosseract"
 	"github.com/oliamb/cutter"
-	"github.com/disintegration/imaging"
+	"github.com/otiai10/gosseract"
 	"image"
 	"image/jpeg"
 	"os"
@@ -29,7 +29,9 @@ func main(){
 		return
 	}
 
-	//err = svc.ExtractFrames("./IMG_1532.MOV", "./", 1)
+	//sourceFile := "./source/IMG_1532.MOV"
+	//sourceFile := "./source/LAE.MOV"
+	//err = svc.ExtractFrames(sourceFile, "./dist", 1)
 	if err != nil {
 		fmt.Println("failed extracting frames")
 		fmt.Println(err)
@@ -38,24 +40,45 @@ func main(){
 	// would be nice to stream the results instead of writing to disk
 
 	// now extract the times
-	f, err := os.Open("frames00017.jpg")
+	f, err := os.Open("./dist/frames00017.jpg")
 	defer f.Close()
 	img, _, err := image.Decode(f)
+	img = imaging.Rotate270(img)
+	//img, err = cutter.Crop(img, cutter.Config{
+	//	Width:  800,
+	//	Height: 170,
+	//	//Anchor: image.Point{580, 340},
+	//	//Mode:   cutter.TopLeft, // optional, default value
+	//	//Width:  80,
+	//	//Height: 60,
+	//	//Anchor: image.Point{585, 345},
+	//	Anchor: image.Point{700, 220},
+	//	Mode:   cutter.TopLeft, // optional, default value
+	//})
+	// 4 numbers we need to get
+
+	//img = imaging.Sharpen(img, 1.5)
+	//img = imaging.Invert(img)
+	//dstImage := imaging.AdjustContrast(img, 15)
+
+	writeImage(1, img, image.Point{700, 220})
+	writeImage(2, img, image.Point{975, 220})
+	writeImage(3, img, image.Point{1220, 220})
+	writeImage(4, img, image.Point{1255, 320})
+	fmt.Println(fmt.Sprintf("power     : %s", getTextFromImage(1)))
+	fmt.Println(fmt.Sprintf("temp      : %s", getTextFromImage(2)))
+	fmt.Println(fmt.Sprintf("impedence : %s", getTextFromImage(3)))
+	fmt.Println(fmt.Sprintf("seconds   : %s", getTextFromImage(4)))
+}
+
+func writeImage(num int, img image.Image, pt image.Point) {
 	croppedImg, err := cutter.Crop(img, cutter.Config{
-		//Width:  300,
-		//Height: 100,
-		//Anchor: image.Point{580, 340},
-		//Mode:   cutter.TopLeft, // optional, default value
-		Width:  80,
-		Height: 60,
-		Anchor: image.Point{585, 345},
-		Mode:   cutter.TopLeft, // optional, default value
+		Width:  100,
+		Height: 100,
+		Anchor:  pt,
+		Mode:   cutter.TopLeft,
 	})
-
-	sharpenedImage := imaging.Sharpen(croppedImg, 1.5)
-	dstImage := imaging.AdjustContrast(sharpenedImage, 15)
-
-	fOut, err := os.Create("out_image.jpg")
+	fOut, err := os.Create(fmt.Sprintf("./dist/out_image%d.jpg", num))
 	if err != nil {
 		panic(err)
 	}
@@ -64,18 +87,17 @@ func main(){
 	opt := jpeg.Options{
 		Quality: 100, // 0-100
 	}
-	err = jpeg.Encode(fOut, dstImage, &opt)
+	err = jpeg.Encode(fOut, croppedImg, &opt)
 	if err != nil {
 		panic(err)
 	}
+}
 
-
-	// the images need to be cropped
+func getTextFromImage(num int) string {
 	client := gosseract.NewClient()
-	//client.SetConfigFile()
-	//client.SetWhitelist("0123456789")
+	client.SetWhitelist("0123456789W")
 	defer client.Close()
-	client.SetImage("out_image.jpg")
+	client.SetImage(fmt.Sprintf("./dist/out_image%d.jpg", num))
 	text, _ := client.Text()
-	fmt.Println(text)
+	return text
 }
