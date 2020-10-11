eeab5486a53f8a44e771c3fcf0dea497667e4cab
mikeeddyUB <meddy22@gmail.com>
2020-10-10 23:25:29 -0400

    mostly works with graph

--- internal/main.go
+++ internal/main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"bytes"
+	"encoding/csv"
 	"fmt"
 	"github.com/disintegration/imaging"
 	"github.com/lijo-jose/gffmpeg/pkg/gffmpeg"
 	"github.com/lijo-jose/goutils/pkg/ffmpeg"
 	"github.com/oliamb/cutter"
 	"github.com/otiai10/gosseract"
+	"github.com/wcharczuk/go-chart"
 	"image"
 	"image/jpeg"
+	"log"
 	"os"
+	"strconv"
 )
 
 
 func main(){
+
+	file, err := os.Create("result.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
 
 	ff, err := gffmpeg.NewGFFmpeg("/usr/bin/ffmpeg")
 	if err != nil {
@@ -21,7 +35,7 @@ func main(){
 		fmt.Println(err)
 		return
 	}
-	//svc, err := ffmpeg.New(ff)
+	svc, err := ffmpeg.New(ff)
 	_, err = ffmpeg.New(ff)
 	if err != nil {
 		fmt.Println("new ffmpeg")
@@ -30,8 +44,8 @@ func main(){
 	}
 
 	//sourceFile := "./source/IMG_1532.MOV"
-	//sourceFile := "./source/LAE.MOV"
-	//err = svc.ExtractFrames(sourceFile, "./dist", 1)
+	sourceFile := "./source/LAE.MOV"
+	err = svc.ExtractFrames(sourceFile, "./dist", 1)
 	if err != nil {
 		fmt.Println("failed extracting frames")
 		fmt.Println(err)
@@ -39,41 +53,136 @@ func main(){
 
 	// would be nice to stream the results instead of writing to disk
 
-	// now extract the times
-	f, err := os.Open("./dist/frames00017.jpg")
-	defer f.Close()
-	img, _, err := image.Decode(f)
-	img = imaging.Rotate270(img)
-	//img, err = cutter.Crop(img, cutter.Config{
-	//	Width:  800,
-	//	Height: 170,
-	//	//Anchor: image.Point{580, 340},
-	//	//Mode:   cutter.TopLeft, // optional, default value
-	//	//Width:  80,
-	//	//Height: 60,
-	//	//Anchor: image.Point{585, 345},
-	//	Anchor: image.Point{700, 220},
-	//	Mode:   cutter.TopLeft, // optional, default value
-	//})
-	// 4 numbers we need to get
+	var impedanceValues []float64
+	var tempValues []float64
+	var secondValues []float64
+	// 00001 -> 00044
+	//for i := 1; i < 45; i++ {
+	for i := 1; i < 15; i++ {
+		file := fmt.Sprintf("./dist/frames%05d.jpg", i)
+		fmt.Println(file)
+		f, err := os.Open(file)
+		if err != nil {
+			panic(err)
+		}
+		img, _, err := image.Decode(f)
+		f.Close()
+		img = imaging.Rotate270(img)
+		writeImage(1, img, image.Point{720, 220})
+		writeImage(2, img, image.Point{975, 220})
+		writeImage(3, img, image.Point{1220, 220})
+		writeImage(4, img, image.Point{1255, 320})
+		power, err := getTextFromImage(1)
+		temp, err := getTextFromImage(2)
+		impedance, err := getTextFromImage(3)
+		seconds, err := getTextFromImage(4)
 
-	//img = imaging.Sharpen(img, 1.5)
-	//img = imaging.Invert(img)
-	//dstImage := imaging.AdjustContrast(img, 15)
+		//secondsNum, err := strconv.Atoi(seconds)
+		fmt.Println("checking condition")
+		fmt.Println(power)
+		fmt.Println(temp)
+		fmt.Println(impedance)
+		fmt.Println(seconds)
+		if len(power) > 0 && len(temp) > 0 && len(impedance) > 0 && len(seconds) > 0 {
+			fmt.Println(fmt.Sprintf("file      : %s", file))
+			fmt.Println(fmt.Sprintf("power     : %s", power))
+			fmt.Println(fmt.Sprintf("temp      : %s", temp))
+			fmt.Println(fmt.Sprintf("impedance : %s", impedance))
+			fmt.Println(fmt.Sprintf("seconds   : %s", seconds))
+			impedanceInt, err := strconv.Atoi(impedance)
+			if err != nil {
+				panic(err)
+			}
 
-	writeImage(1, img, image.Point{700, 220})
-	writeImage(2, img, image.Point{975, 220})
-	writeImage(3, img, image.Point{1220, 220})
-	writeImage(4, img, image.Point{1255, 320})
-	fmt.Println(fmt.Sprintf("power     : %s", getTextFromImage(1)))
-	fmt.Println(fmt.Sprintf("temp      : %s", getTextFromImage(2)))
-	fmt.Println(fmt.Sprintf("impedence : %s", getTextFromImage(3)))
-	fmt.Println(fmt.Sprintf("seconds   : %s", getTextFromImage(4)))
+			tempInt, err := strconv.Atoi(temp)
+			if err != nil {
+				panic(err)
+			}
+
+			secondInt, err := strconv.Atoi(seconds)
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Println(impedance)
+			fmt.Println(impedanceInt)
+			fmt.Println(float64(impedanceInt))
+			impedanceValues = append(impedanceValues, float64(impedanceInt))
+			tempValues = append(tempValues, float64(tempInt))
+			secondValues = append(secondValues, float64(secondInt))
+			// impedance over time (primary y axis)
+			// temperature over time (secondary y axis)
+
+			//var data = []string{power, temp, impedance, seconds}
+			//err = writer.Write(data)
+			//if err != nil {
+			//	panic(err)
+			//}
+		}
+
+		//impedanceSeries := chart.ContinuousSeries{
+		//	XValues: secondValues,
+		//	YValues: impedanceValues,
+		//}
+
+		//graph := chart.Chart{
+		//	Series: []chart.Series{
+		//		chart.ContinuousSeries{
+		//			XValues: []float64{1.0, 2.0, 3.0, 4.0},
+		//			YValues: []float64{1.0, 2.0, 3.0, 4.0},
+		//		},
+		//	},
+		//}
+		//
+		//buffer := bytes.NewBuffer([]byte{})
+		//err = graph.Render(chart.PNG, buffer)
+		//if err != nil {
+		//	panic(err)
+		//}
+	}
+	fmt.Println(impedanceValues)
+	fmt.Println(tempValues)
+	fmt.Println(secondValues)
+	tempSeries := chart.ContinuousSeries{
+		XValues: secondValues,
+		YValues: tempValues,
+	}
+
+	graph := chart.Chart{
+		XAxis: chart.XAxis{
+			Name: "Time",
+		},
+		//YAxisSecondary:
+		YAxis: chart.YAxis{
+			Name: "Temperature",
+		},
+		//Series: []chart.Series{impedanceSeries, tempSeries},
+		Series: []chart.Series{tempSeries},
+	}
+
+	buffer := bytes.NewBuffer([]byte{})
+	err = graph.Render(chart.PNG, buffer)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(buffer.Len())
+	im, _, err := image.Decode(bytes.NewReader(buffer.Bytes()))
+	out, _ := os.Create("./img.jpg")
+	defer out.Close()
+
+	var opts jpeg.Options
+	opts.Quality = 100
+
+	err = jpeg.Encode(out, im, &opts)
+	//jpeg.Encode(out, img, nil)
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 func writeImage(num int, img image.Image, pt image.Point) {
 	croppedImg, err := cutter.Crop(img, cutter.Config{
-		Width:  100,
+		Width:  120,
 		Height: 100,
 		Anchor:  pt,
 		Mode:   cutter.TopLeft,
@@ -93,11 +202,12 @@ func writeImage(num int, img image.Image, pt image.Point) {
 	}
 }
 
-func getTextFromImage(num int) string {
+func getTextFromImage(num int) (string, error) {
 	client := gosseract.NewClient()
 	client.SetWhitelist("0123456789W")
 	defer client.Close()
 	client.SetImage(fmt.Sprintf("./dist/out_image%d.jpg", num))
 	text, _ := client.Text()
-	return text
+	//return strconv.Atoi(text)
+	return text, nil
 }
